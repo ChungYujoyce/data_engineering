cb26b777182c472cb8ba804df08a5e4a689afa4d
jj <joyce.bhps@gmail.com>
2023-05-06 12:40:29 -0700

    change encoding

--- kafka_spark/kafka_producer.py
+++ kafka_spark/kafka_producer.py
@@ -2,7 +2,7 @@ import pandas as pd
 from kafka import KafkaProducer
 from datetime import datetime
 import time
-import random
+from json import dumps
 import numpy as np
 
 KAFKA_TOPIC_NAME_CONS = "songTopic"
@@ -12,15 +12,16 @@ if __name__ == '__main__':
     print("Kafka Producer Application Started ... ")
 
     kafka_producer_obj = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS_CONS,
-                                        value_serializer=lambda x:x.encode('utf-8'))
+                                        value_serializer=lambda x: dumps(x).encode('utf-8'))
     songs_df = pd.read_csv('./my_songs.csv')
     songs_df = songs_df[songs_df['popularity'] > 75]
     song_list = songs_df.to_dict(orient="records")
     
     message_list = []
-    value_name = ['name', 'artists', 'popularity', 'duration_s','acousticness', 'speechiness', 'key', 'liveness', 'danceability', 
-                'instrumentalness', 'energy','tempo', 'time_signature', 'loudness', 'valence']
-
+    value_name = ['name', 'artists', 'popularity', 'duration_s', 'acousticness',
+                  'danceability', 'energy', 'loudness', 'speechiness',  
+                  'instrumentalness', 'liveness', 'valence' ,'tempo', 'time_signature']
+                          
     message = None
     for song in song_list:
         message_list_value = []
